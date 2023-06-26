2e316e3b9529d0e7c64348cd95e93c350dfcce10
grrowerr <rslvkirsanov@gmail.com>
2023-06-26 16:08:46 +0300

    add test_tile, tiles.py, main_settings.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame, sys
+from main_settings import *
+from tiles import Tile
+
+pygame.init()
+main_win = pygame.display.set_mode((scr_width, scr_height))
+clock = pygame.time.Clock()
+test_tile = pygame.sprite.Group(Tile((100, 100), 200))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill('black')
+    test_tile.draw(main_win)
+
+    pygame.display.update()
+    clock.tick(60)
---
+++ main_settings.py
@@ -0,0 +1,19 @@
+level_map = [
+    '                              ',
+    '                              ',
+    '                              ',
+    ' XX    XXX              XXX   ',
+    ' XX                  X        ',
+    ' XXXX           XXX        XX ',
+    ' XXXX       XX          X     ',
+    ' XX    X  XXXX     XX  XXX    ',
+    '       X  XXXX     XX  XXXX   ',
+    '    XXXX  XXXXXXX  XX  XXXXX  ',
+    'XXXXXXXX  XXXXXXX  XX  XXXXX  '
+]
+
+tile_size = 64
+scr_width = 1400
+scr_height = len(level_map) * tile_size
+
+
