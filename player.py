326167c2675ba0f17be0dfeabc831d2355cefa46
grrowerr <rslvkirsanov@gmail.com>
2023-07-03 04:18:25 +0300

    - update animation - add direction animation for player

--- player.py
+++ player.py
@@ -16,6 +16,14 @@ class Player(pygame.sprite.Sprite):
         self.gravity = 0.8
         self.jump_speed = -16
 
+        #player status
+        self.character_status = 'idle'
+        self.facing_right = True
+        self.on_ground = False
+        self.on_ceiling = False
+        self.on_left = False
+        self.on_right = False
+
     def import_char_assets(self):
         character_path = 'graphics/character/'
         self.char_animations = {'idle': [], 'run': [], 'jump': [], 'fall': []}
@@ -23,18 +31,56 @@ class Player(pygame.sprite.Sprite):
         for animation in self.char_animations.keys():
             full_path = character_path + animation
             self.char_animations[animation] = import_folder(full_path)
+
+    def animate(self):
+        animation = self.char_animations[self.character_status]
+
+        #loop over frame index
+        self.frame_index += self.animation_speed
+        if self.frame_index >= len(animation):
+            self.frame_index = 0
+
+        image = animation[int(self.frame_index)]
+        if self.facing_right:
+            self.image = image
+        else:
+            flipped_image = pygame.transform.flip(image, True, False)
+            self.image = flipped_image
+
+        #set rectangle
+        if self.on_ground:
+            self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
+        elif self.on_ceiling:
+            self.rect = self.image.get_rect(midtop = self.rect.midtop)
+        else:
+            self.rect = self.image.get_rect(center = self.rect.center)
+
+
     def get_input_data(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.facing_right = True
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.facing_right = False
         else:
             self.direction.x = 0
 
         if keys[pygame.K_SPACE]:
             self.jump()
+
+    def get_char_status(self):
+        if self.direction.y < 0:
+            self.character_status = 'jump'
+        elif self.direction.y > 1:
+            self.character_status = 'fall'
+        elif self.direction.x != 0:
+            self.character_status = 'run'
+        else:
+            self.character_status = 'idle'
+
 
     def apply_gravity(self):
         self.direction.y += self.gravity
@@ -45,6 +91,8 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.get_input_data()
+        self.get_char_status()
+        self.animate()
 
 
 
